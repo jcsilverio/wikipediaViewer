ca1952d57de525dea2000e01403ce86bd8b06ae8
JC Silverio <silverio@impulsevfx.com>
2017-10-01 15:17:19 -0400

    listing populate

--- app.js
+++ app.js
@@ -1,62 +1,76 @@
 var usersSearch;
+var results;
 $("#searchButton").click(function() {
   usersSearch = document.getElementById('searchInput').value;
-   console.log('usersSearch: ',usersSearch);
-   if(!$.trim(($("#searchInput")).value).length) { // string  exists AFTER a trim
+  console.log('usersSearch: ', usersSearch);
+  if (!$.trim(($("#searchInput")).value).length) { // string  exists AFTER a trim
 
-  $.ajax({
-    type: "GET",
-    url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + usersSearch +"&callback=?",
-    contentType: "application/json; charset=utf-8",
-    async: true,
-    dataType: "json",
-    success: function(data, textStatus, jqXHR) {
-      console.log('data: ', data);
+    $.ajax({
+      type: "GET",
+      url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + usersSearch + "&callback=?",
+      contentType: "application/json; charset=utf-8",
+      async: true,
+      dataType: "json",
+      success: function(data, textStatus, jqXHR) {
+        //console.log('data: ', data);
 
-      //var markup = data.parse.text["*"];
-      //var blurb = $('<div class="card animated bounceInLeft" width="100%" height="500px" frameborder="0"></div>').html(markup);
+        //var markup = data.parse.text["*"];
+        //var blurb = $('<div class="card animated bounceInLeft" width="100%" height="500px" frameborder="0"></div>').html(markup);
 
-      // remove links as they will not work
-     // blurb.find('a').each(function() { $(this).replaceWith($(this).html()); });
+        // remove links as they will not work
+        // blurb.find('a').each(function() { $(this).replaceWith($(this).html()); });
 
-      // remove any references
-     // blurb.find('sup').remove();
+        // remove any references
+        // blurb.find('sup').remove();
 
-      // remove cite error
-      //blurb.find('.mw-ext-cite-error').remove();
-    //  $('#wikiViewer').html($(blurb).find('p'));
+        // remove cite error
+        //blurb.find('.mw-ext-cite-error').remove();
+        //  $('#wikiViewer').html($(blurb).find('p'));
+      results = data.query.pages;
+      var pageNum;
+      var pageId = '<a href="https://en.wikipedia.org/?curid=' + pageNum;
+      var  $searchListingBegin = '<a href="#" class="list-group-item"><h4 class="list-group-item-heading">';
+      var heading;
+      var $searchListingMid = '</h4><p class="list-group-item-text">';
+      var extract;
+      var $searchListingEnd = '</p></a>';
+      for (var key in results) {
+        heading = results[key].title;
+        extract = results[key].extract;
+        console.log('results key: ', results[key].title);
+        var $listResult = $searchListingBegin + heading + $searchListingMid + extract  +$searchListingEnd;
 
-    },
-    error: function(errorMessage) {}
-  });
-     } else {
+        $("#searchResults").append($listResult);
+      }
+
+
+      console.log('results: ',results);
+
+
+      },
+      error: function(errorMessage) {}
+    });
+  } else {
     return;
-   }
+  }
 });
 
-
-
-
 /*
-    url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" + usersSearch +"&callback=?",
+$scope.search = function() {
+  $scope.results = [];
+  help.addClass('hide');
+  search.removeClass('fullHeight');
+  var title = input.val();
+  var api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+  var cb = '&callback=JSON_CALLBACK';
+  var page = 'https://en.wikipedia.org/?curid=';
 
-
-<body
-  <div class="container-fluid text-center">
-    <div class="row">
-      <div class="col-sm-4"></div>
-      <div class="col-sm-4">
-        <div class="card" style="width:375px; height:375px">
-          <img class="card-img-top" style="opacity:0.4; width:auto; height:200px" id="quoteImage" alt="nature image">
-          <div class="card-block">
-            <h4 class="card-title quote-text" id="text"></h4>
-            <h6 class="card-subtitle mb-2 text-muted quote-author" id="author"></h6>
-            <a href="#" class="btn btn-secondary" id="tweetBtn" style="opacity:0;"><i class="fa fa-twitter"></i></a>
-    <div class="list-group" id="searchResults">
-      <a href="#" class="list-group-item">
-        <h4 class="list-group-item-heading">List group item heading</h4>
-        <p class="list-group-item-text">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-      </a>
-      </div>
-
-      */
+  $http.jsonp(api + title + cb)
+    .success(function(data) {
+      var results = data.query.pages;
+      angular.forEach(results, function(v, k) {
+        $scope.results.push({ title: v.title, body: v.extract, page: page + v.pageid })
+      })
+    });
+}
+*/
