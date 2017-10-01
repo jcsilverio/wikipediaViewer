6b5e0bf6122a872c211b9d60abfef4c96d2cadb8
JC Silverio <silverio@impulsevfx.com>
2017-10-01 14:25:41 -0400

    rearrangement of windows, update nav bar

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+var usersSearch;
 $("#searchButton").click(function() {
+  usersSearch = document.getElementById('searchInput').value;
+   console.log('usersSearch: ',usersSearch);
+   if(!$.trim(($("#searchInput")).value).length) { // string  exists AFTER a trim
 
   $.ajax({
     type: "GET",
-    url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?",
+    url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" + usersSearch +"&callback=?",
     contentType: "application/json; charset=utf-8",
     async: false,
     dataType: "json",
@@ -10,7 +14,7 @@ $("#searchButton").click(function() {
       console.log(data);
 
       var markup = data.parse.text["*"];
-      var blurb = $('<div></div>').html(markup);
+      var blurb = $('<div class="card animated bounceInLeft" width="100%" height="500px" frameborder="0"></div>').html(markup);
 
       // remove links as they will not work
       blurb.find('a').each(function() { $(this).replaceWith($(this).html()); });
@@ -20,15 +24,33 @@ $("#searchButton").click(function() {
 
       // remove cite error
       blurb.find('.mw-ext-cite-error').remove();
-      $('#searchResults').html($(blurb).find('p'));
+      $('#wikiViewer').html($(blurb).find('p'));
 
     },
     error: function(errorMessage) {}
   });
+     } else {
+    return;
+   }
 });
 
 
+
+
 /*
+
+
+<body
+  <div class="container-fluid text-center">
+    <div class="row">
+      <div class="col-sm-4"></div>
+      <div class="col-sm-4">
+        <div class="card" style="width:375px; height:375px">
+          <img class="card-img-top" style="opacity:0.4; width:auto; height:200px" id="quoteImage" alt="nature image">
+          <div class="card-block">
+            <h4 class="card-title quote-text" id="text"></h4>
+            <h6 class="card-subtitle mb-2 text-muted quote-author" id="author"></h6>
+            <a href="#" class="btn btn-secondary" id="tweetBtn" style="opacity:0;"><i class="fa fa-twitter"></i></a>
     <div class="list-group" id="searchResults">
       <a href="#" class="list-group-item">
         <h4 class="list-group-item-heading">List group item heading</h4>
