df9358a46b4bb9b81a89f63e8505fe9350469fce
JC Silverio <silverio@impulsevfx.com>
2017-10-01 14:36:58 -0400

    getting proper data

--- app.js
+++ app.js
@@ -6,25 +6,25 @@ $("#searchButton").click(function() {
 
   $.ajax({
     type: "GET",
-    url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" + usersSearch +"&callback=?",
+    url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + usersSearch +"&callback=?",
     contentType: "application/json; charset=utf-8",
-    async: false,
+    async: true,
     dataType: "json",
     success: function(data, textStatus, jqXHR) {
-      console.log(data);
+      console.log('data: ', data);
 
-      var markup = data.parse.text["*"];
-      var blurb = $('<div class="card animated bounceInLeft" width="100%" height="500px" frameborder="0"></div>').html(markup);
+      //var markup = data.parse.text["*"];
+      //var blurb = $('<div class="card animated bounceInLeft" width="100%" height="500px" frameborder="0"></div>').html(markup);
 
       // remove links as they will not work
-      blurb.find('a').each(function() { $(this).replaceWith($(this).html()); });
+     // blurb.find('a').each(function() { $(this).replaceWith($(this).html()); });
 
       // remove any references
-      blurb.find('sup').remove();
+     // blurb.find('sup').remove();
 
       // remove cite error
-      blurb.find('.mw-ext-cite-error').remove();
-      $('#wikiViewer').html($(blurb).find('p'));
+      //blurb.find('.mw-ext-cite-error').remove();
+    //  $('#wikiViewer').html($(blurb).find('p'));
 
     },
     error: function(errorMessage) {}
@@ -38,6 +38,7 @@ $("#searchButton").click(function() {
 
 
 /*
+    url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" + usersSearch +"&callback=?",
 
 
 <body
