60cb5988163cec2c1c8b9fabc5ecc6e7c6452598
JC Silverio <silverio@impulsevfx.com>
2017-10-01 21:59:36 -0400

    pages loading in both features

--- app.js
+++ app.js
@@ -1,49 +1,109 @@
 var usersSearch;
 var results;
+var listInfo = [];
+var selectedListItem;
+var pageNum;
+var id;
+var updateFrameHTML;
+
+$("#searchResults").on('click', '.list-group-item', getRealPage);
+
+//if id clicked equals matching id in list info
+//populate main page with associated link using iframe
+function getRealPage() {
+  id = Number($(this).attr('id'));
+  console.log('getRealPage pageNum: ', listInfo[id]["$pageIdNum"]);
+  var listPageNum = listInfo[id]["$pageIdNum"];
+
+  $.ajax({
+    type: "GET",
+     url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + usersSearch + "&prop=info&inprop=url&callback=?",
+    contentType: "application/json; charset=utf-8",
+    async: true,
+    dataType: "json",
+    success: function(data, textStatus, jqXHR) {
+
+      var pageResults = data.query.pages;
+      console.log('real page data: ', pageResults);
+      for(var key in pageResults){
+
+       if (Number(key) === listPageNum){
+
+        var fullurl = pageResults[key]['fullurl'];
+        updateFrameHTML = '<iframe class="animated bounceInLeft" width="100%" height="500px" frameborder="0" src="' + fullurl +'"></iframe>';
+
+      $("#wikiViewer").html(updateFrameHTML);
+
+       }
+      }
+
+
+//&prop=info&inprop=url
+    }
+  });
+}
+/*
+function updateFrame() {
+  id = Number($(this).attr('id'));
+  console.log('updateFrame id: ', id);
+  getRealPage();
+  for (var key in listInfo) {
+
+    if (listInfo[key].listIndex === id) {
+      updateFrameHTML = '<iframe class="animated bounceInLeft" width="100%" height="500px" frameborder="0" src="https://en.wikipedia.org/wiki/The_Jimi_Hendrix_Experience"></iframe>';
+
+
+      //updateFrameHTML = '<iframe class="animated bounceInLeft" width="100%" height="500px" frameborder="0" src="https://en.wikipedia.org/?curid="' + pageNum +'></iframe>';
+
+      //$("#wikiViewer").html(updateFrameHTML);
+    }
+
+  }
+
+  console.log('listInfo[key]', listInfo[key]);
+}
+*/
+
 $("#searchButton").click(function() {
   usersSearch = document.getElementById('searchInput').value;
   console.log('usersSearch: ', usersSearch);
   if (!$.trim(($("#searchInput")).value).length) { // string  exists AFTER a trim
-
     $.ajax({
       type: "GET",
+
       url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + usersSearch + "&callback=?",
       contentType: "application/json; charset=utf-8",
       async: true,
       dataType: "json",
       success: function(data, textStatus, jqXHR) {
-        //console.log('data: ', data);
 
-        //var markup = data.parse.text["*"];
-        //var blurb = $('<div class="card animated bounceInLeft" width="100%" height="500px" frameborder="0"></div>').html(markup);
-
-        // remove links as they will not work
-        // blurb.find('a').each(function() { $(this).replaceWith($(this).html()); });
-
-        // remove any references
-        // blurb.find('sup').remove();
-
-        // remove cite error
-        //blurb.find('.mw-ext-cite-error').remove();
-        //  $('#wikiViewer').html($(blurb).find('p'));
-        $("#searchResults").html(' ');
+        $("#searchResults").html('  ');
         results = data.query.pages;
-        var pageNum;
-        var $pageId;
-        var $searchListingBegin = ' class="list-group-item"><h4 class="list-group-item-heading">';
+        console.log('first results: ', results);
+
+
+        var $searchListingBegin;
         var heading;
         var $searchListingMid = '</h4><p class="list-group-item-text">';
         var extract;
-        var $searchListingEnd = '</p></a>';
+        var $searchListingEnd = '</p></span>';
+        var index = -1;
         for (var key in results) {
+          index++;
+          $searchListingBegin = '<span class="list-group-item"' + 'id=' + index + '><h4 class="list-group-item-heading">';
+
           pageNum = results[key].pageid;
-          $pageId = '<a href="https://en.wikipedia.org/?curid=' + pageNum + '"';
+
           heading = results[key].title;
           extract = results[key].extract;
 
-          var $listResult = $pageId + $searchListingBegin + heading + $searchListingMid + extract + $searchListingEnd;
+          var $listResult = $searchListingBegin + heading + $searchListingMid + extract + $searchListingEnd;
 
           $("#searchResults").append($listResult);
+
+          listInfo.push({ "listIndex": index, "$pageIdNum": pageNum, "pageHeading": heading });
+
+
         }
 
         console.log('results: ', results);
